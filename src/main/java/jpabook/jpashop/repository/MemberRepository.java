3ea8f3dfad8da2f6d9d7283f82518bb6e0f59cdc
talk105 <talk105@naver.com>
2023-06-27 22:17:04 +0900

    Member Repository

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository // 자동으로 스프링 빈 관리 (Repository는 저장소를 의미한다) , Component를 포함하고 있다.
+@RequiredArgsConstructor
+public class MemberRepository {
+    // @PersistenceContext // JPA 표준 어노테이션
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class).setParameter("name", name).getResultList();
+    }
+}
