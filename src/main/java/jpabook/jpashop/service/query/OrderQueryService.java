03d8273476f2318b6a23a6a94ba62bd4255255f5
talk105 <talk105@naver.com>
2023-07-03 00:01:24 +0900

    Order Query Service

---
+++ src/main/java/jpabook/jpashop/service/query/OrderQueryService.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.service.query;
+
+import jpabook.jpashop.api.OrderApiController;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+// Transactional 이 있기 때문에 OSIV (Open Session In VIEW)를 꺼도 영속성 컨텍스트가 유지가 되는거임.
+// 별도 Service 로 분리.
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderQueryService {
+    private final OrderRepository orderRepository;
+
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    public List<OrderDto> ordersV3_page(int offset, int limit) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+}
